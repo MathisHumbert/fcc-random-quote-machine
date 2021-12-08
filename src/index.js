5d05921691c96b5bdd744784ddffca5580f74804
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-12-08 10:36:02 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const url = `https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json`;
+
+const App = () => {
+  const [quote, setQuote] = useState([]);
+
+  const fetchData = async () => {
+    const response = fetch(url);
+    const data = response.json();
+    console.log(data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <main id="quote-box">
+      <div className="container">
+        <h1></h1>
+      </div>
+    </main>
+  );
+};
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,8 +32,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
